458b9e357429e77038614de3b806490a6dea31e2
Siternet <siternet.21@gmail.com>
2025-09-10 16:29:42 -0500

    Exposición del método init de la instancia SocialProof

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 declare global {
   interface Window {
     SocialProof: {
+      init({ businessKey: string }): void;
       pushEvent(eventName: string, parameters: Record<string, any>): void;
       firePopup(id: number, title: string, message: string): void;
       showPopup(title: string, message: string): void;
